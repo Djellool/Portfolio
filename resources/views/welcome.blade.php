c7efa005b2ffeaf7538ba5db4b48cf92d204e950
Boutouili Djillali <gd_boutouili@esi.dz>
2021-08-21 16:49:11 +0100

    new commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,9 +15,9 @@
     <link href="{{asset('lib/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
 
     <!-- Libraries CSS Files -->
-    <link href="{{asset('lib/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{asset('font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
     <link href="{{asset('lib/animate/animate.min.css')}}" rel="stylesheet">
-    <link href="{{asset('lib/ionicons/css/ionicons.min.css')}}" rel="stylesheet">
+    <link href="{{asset('ionicons/css/ionicons.min.css')}}" rel="stylesheet">
     <link href="{{asset('lib/owlcarousel/assets/owl.carousel.min.css')}}" rel="stylesheet">
     <link href="{{asset('lib/lightbox/css/lightbox.min.css')}}" rel="stylesheet">
 
